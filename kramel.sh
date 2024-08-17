c3eec90a34854762ff4b209066b94edbc97998b1
Robin <hanya8493@gmail.com>
2024-08-17 14:57:58 +0000

    Kramel: Refactor module handling in build script
    Signed-off-by: Robin <hanya8493@gmail.com>

--- kramel.sh
+++ kramel.sh
@@ -115,11 +115,14 @@ if [ "${ci}" != 1 ]; then
     if [ -z "${zipn}" ]; then
         echo -ne "\e[1mEnter zipname: \e[0m"
         read -r zipn
-	if [[ "${MODULE}" = "1" ]]; then
-        echo -ne "\e[1mEnter modulename: \e[0m"
-	read -r modn
+   fi
+
+   if [ "${MODULE}" = "1" ]; then
+       if [ -z "${modn}" ]; then
+           echo -ne "\e[1mEnter modulename: \e[0m"
+	   read -r modn
+       fi
     fi
-fi
 else
     export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
     export KBUILD_BUILD_HOST=$DRONE_SYSTEM_HOST
@@ -127,7 +130,7 @@ else
     export VERSION=$version
     kver=$KBUILD_BUILD_VERSION
     zipn=Nethunter-a23-${VERSION}
-    if [[ "${MODULE}" = "1" ]]; then
+    if [ "${MODULE}" = "1" ]; then
         modn="${zipn}-modules"
     fi
 fi
